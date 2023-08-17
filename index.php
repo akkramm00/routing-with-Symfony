e4c89d026e35b84764c6d21bd0cd9a6d4a2c4c6e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-17 19:11:10 +0000

    add namespace App\Controller

--- index.php
+++ index.php
@@ -34,6 +34,28 @@ kernel:
 resource: App\Kernel
 type: attribute
       ?>
-This configuration tells Symfony to look for routes defined as attributes on classes declareted in the App\Controller namespace and stored in the src/Controller directory witch follows the PSR-4 standard. The kkernel can act as a controller too, witch is especially useful for small that use Symfony as a microframework
+<p>
+This configuration tells Symfony to look for routes defined as attributes on classes declareted in the App\Controller namespace and stored in the src/Controller directory witch follows the PSR-4 standard. The kkernel can act as a controller too, witch is especially useful for small that use Symfony as a microframework..
+
+  Suppose you want to define a route for the /blog URL in your application. To do so, create a "controller class" like the following:
+  <?php
+//src/Controller/BlogController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstracController
+  {
+    #[Route('/blog' , name:'blog_list')]
+    public function list(): Response
+    {
+      //...
+    }
+  }
+?>
+
+  This configuration defines a route called "blog_list" that matches when the user requests the /blog URL. When the match occurs, the application runs the list() method of the BlogController class.
+</p>
   </body>
 </html>
